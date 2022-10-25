e95f537105d9432aafea735c7c320c9595f79e40
Hellen Avila Rosa <39311424+hellenavilarosa@users.noreply.github.com>
2022-10-25 18:21:53 -0300

    change maps version

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_6WRe0jrae', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '36431ulvlkpm9jmn3mqdfh4g3k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://hbg0u6w8xc.execute-api.us-east-2.amazonaws.com/das' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
